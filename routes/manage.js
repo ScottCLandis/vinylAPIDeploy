b0d3877a0d759278ef797b599ab880eccb1f73fe
scottclandis <landis@work.co>
2025-10-07 16:45:29 -0400

    release date and genre added

--- routes/manage.js
+++ routes/manage.js
@@ -1,23 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const app = express()
-const port = 3000
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
 var dbConn = mysql.createConnection({
-    host: "mysql.app.scottclandis.com",
-    user: "scottclandis",
-    password: "Gordan24",
-    database: "vinyl_collection"
+  host: "mysql.app.scottclandis.com",
+  user: "scottclandis",
+  password: "Gordan24",
+  database: "vinyl_collection",
 });
 
-router.post('/', function(req, res, next) {
-  console.log("manage")
+router.post("/", function (req, res, next) {
+  console.log("manage");
 });
 
-router.post('/addToCollection', (req, res) => {
-
+router.post("/addToCollection", (req, res) => {
   var album = [
     req.body.artist,
     req.body.album_name,
@@ -27,41 +26,40 @@ router.post('/addToCollection', (req, res) => {
     req.body.color,
     req.body.size,
     req.body.notes,
-    req.body.custom_img
-    ];
+    req.body.custom_img,
+    req.body.genre,
+    req.body.releaseDate,
+  ];
 
-    var dup = {
-      dupResponse: 'duplicate'  
-    }
-  dbConn.query("INSERT INTO collection (artist, album_name, album_art, category, mbid, color, size, notes, custom_img) VALUES ( ? )", [album],
-  function (error, results) {
-    console.log("error test", error)
+  var dup = {
+    dupResponse: "duplicate",
+  };
+  dbConn.query(
+    "INSERT INTO collection (artist, album_name, album_art, category, mbid, color, size, notes, custom_img, genre, releaseDate) VALUES ( ? )",
+    [album],
+    function (error, results) {
+      console.log("error test", error);
 
-    if (error){
-      if (error.errno === 1062){
-        console.log('dupe')
+      if (error) {
+        if (error.errno === 1062) {
+          console.log("dupe");
+          return res.send({
+            data: "dupe",
+          });
+        } else {
+          return res.send({
+            data: error.code,
+          });
+        }
+      } else {
         return res.send({
-         
-          data: 'dupe'
-         
-        })
-      }  
-      else{
-        return res.send({
-          data: error.code
-        })
-      }
-    }
-    else{
-     
-      return res.send({
           error: false,
           data: results,
-          message: 'New records has been created successfully.'
-      });
+          message: "New records has been created successfully.",
+        });
+      }
     }
-  });
-
+  );
 });
 
 module.exports = router;
